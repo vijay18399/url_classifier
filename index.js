046c0af67a93124aa1f570b1d85db66b213a0e5e
vijay18399 <vijayreddy18399@gmail.com>
2020-03-12 19:03:55 +0530

    added

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ var BayesClassifier = require('bayes-classifier');
 var classifier = new BayesClassifier();
 var fs = require('fs');
 var good = require('./good');
-var bad = require('./bad');
-//console.log(good);
-//console.log(bad);
- 
+var bad = require('./bad'); 
 classifier.addDocuments(good.good, `good`)
+console.log("Learning Good Urls Completed") 
 classifier.addDocuments(bad.bad, `bad`)
+console.log("Learning Bad Urls Completed") 
+console.log("Training Strated") 
 classifier.train()
+console.log("training Completed") 
 console.log(classifier.classify(`google.com`)) 
 var storeFile = `${__dirname}/model.json`
 fs.writeFileSync(storeFile, JSON.stringify(classifier))
+console.log("Execution Completed") 
  
